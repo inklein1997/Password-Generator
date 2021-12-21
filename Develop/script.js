3b31b1299e7fccc5658efcce54986380005468b3
Michael Klein <Michaelklein1997@gmail.com>
2021-12-21 16:53:43 -0600

    added to pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -14,23 +14,31 @@ function writePassword() {
 
 //Click button to generate password
 
-//Responses will be stored in object called userResponses
+//generatePassword function will begin.
+
+//Prompts will display using prompt().  Responses will be stored in object called userResponses
 
 //Prompt #1 = How many characters would you like for your password to have?  Must have a minimum of 8 and a maximum of 128.
 //variable = passwordLength
 
 //Prompt #2 = Would you like for your password to have uppercase letters?
 //array uppercase = [A, B, C, D, ... X, Y, Z]
+//if yes, concat with array password = []
 
 //Prompt #3 = Would you like for your password to have lowercase letters?
 //array lowercase = [a, b, c, d, ... x, y, z]
+//if yes, concat with array password = []
 
 //Prompt #4 = Would you like for your password to have numbers?
 //array numbers = [1, 2, 3, 4 ... 8, 9, 0]
+//if yes, concat with array password = []
 
 //Prompt #5 = Would you like for your password to have special characters?
 //array special = [!, ", #, $, %, &, ', ( ... +, -, ^]
+//if yes, concat with array password = []
 
+//use to loop to pull a single character from array password "i" or "passwordLength" times.
+//ensure that every pull is random using math.random().
 
 
 // Add event listener to generate button
