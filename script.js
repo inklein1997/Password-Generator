686d8153e2d04e41ae586e11cabe1369aa51d555
Michael Klein <Michaelklein1997@gmail.com>
2021-12-22 10:06:52 -0600

    refactored javascript code

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//PSEUDO CODE
+
+  //generatePassword function will begin after clicking button.
+  // Prompts will display using prompt().  Responses will be stored in object called userResponses
+  // Prompt #1 = How many characters would you like for your password to have?  Must have a minimum of 8 and a maximum of 128.
+  // Prompt #2 = Would you like for your password to have uppercase letters? / if yes, concat with array password = []
+  // Prompt #3 = Would you like for your password to have lowercase letters? / if yes, concat with array password = []
+  // Prompt #4 = Would you like for your password to have numbers / if yes, concat with array password = []
+  // Prompt #5 = Would you like for your password to have special characters? / if yes, concat with array password = []
+  // use to loop to pull a single character from array password "i" or "passwordLength" times. / ensure that every pull is random using math.random().
+  // Add event listener to generate button
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordLength;
@@ -5,18 +17,14 @@ const uppercase = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 const lowercase = uppercase.map(name => name.toLowerCase());  // console.log(lowercase)
 const numbers = [0,1,2,3,4,5,6,7,8,9,0]
 const special = ["!", "'", '"', "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".","/",";",":","<",">","=","?","@","[","]","\ ","{","}", "|","~","`"]
-// let passwordArray = [];
-// let randomPassword = "";
 
 
-generateBtn.addEventListener("click", writePassword);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-
 }
 
 function specifyLength() {
@@ -29,10 +37,6 @@ function specifyLength() {
     }
     return passwordLength
   }
-
-//PSEUDO CODE
-
-//Click button to generate password
 
 function generatePassword() {
   var Length = specifyLength();
@@ -90,31 +94,4 @@ function generatePassword() {
   return randomPassword
   }
 
-  
-//generatePassword function will begin.
-
-//Prompts will display using prompt().  Responses will be stored in object called userResponses
-
-//Prompt #1 = How many characters would you like for your password to have?  Must have a minimum of 8 and a maximum of 128.
-//variable = passwordLength
-
-//Prompt #2 = Would you like for your password to have uppercase letters?
-//array uppercase = [A, B, C, D, ... X, Y, Z]
-//if yes, concat with array password = []
-
-//Prompt #3 = Would you like for your password to have lowercase letters?
-//array lowercase = [a, b, c, d, ... x, y, z]
-//if yes, concat with array password = []
-
-//Prompt #4 = Would you like for your password to have numbers?
-//array numbers = [1, 2, 3, 4 ... 8, 9, 0]
-//if yes, concat with array password = []
-
-//Prompt #5 = Would you like for your password to have special characters?
-//array special = [!, ", #, $, %, &, ', ( ... +, -, ^]
-//if yes, concat with array password = []
-
-//use to loop to pull a single character from array password "i" or "passwordLength" times.
-//ensure that every pull is random using math.random().
-
-// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
